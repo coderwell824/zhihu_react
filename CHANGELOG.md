91309a9ada36a6900ad2205bc973bf5bd349e275
coderwell <90953900+coderwell824@users.noreply.github.com>
2023-08-05 09:55:09 +0800

    fixed:init

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# 0.0.0 (2023-08-05)
+
+
+### Bug Fixes
+
+* change file ([522862e](https://github.com/coderwell824/zhihu_react/commit/522862eee16f549281d453d52d141ecd0e1019d7))
+
+
+
--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
-import { BrowserRouter } from "react-router-dom";
-import { KeepAliveProvider } from "keepalive-react-component";
-import RouterView from  "@/router"
+import { BrowserRouter } from 'react-router-dom';
+import { KeepAliveProvider } from 'keepalive-react-component';
+import RouterView from '@/router';
 
-const App = () => {
-  return (
-    <BrowserRouter>
-      <KeepAliveProvider>
-        <RouterView/>
-      </KeepAliveProvider>
-    </BrowserRouter>
-  );
-};
+const App = () => (
+  <BrowserRouter>
+    <KeepAliveProvider>
+      <RouterView />
+    </KeepAliveProvider>
+  </BrowserRouter>
+);
 
 export default App;
+
+// import { RouterProvider } from 'react-router-dom';
+// import router from './router/index.tsx';
+
+// function App() {
+//   return <RouterProvider router={router} />;
+// }
+
+// export default App;
